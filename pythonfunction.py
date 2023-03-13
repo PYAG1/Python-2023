4d4fccaed6500e56671602e5278131cecaef5e22
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-13 18:59:19 +0000

    Update pythonfunction.py

--- pythonfunction.py
+++ pythonfunction.py
@@ -1,3 +1,5 @@
+##functions in python
+
 bill = 175.00
 
 tax_rate = 15
@@ -7,3 +9,8 @@ def billrate(bill,tax_rate):
     print(total)
 
 billrate(bill, tax_rate)
+
+ #LEGB 
+ #Local Enclosed Global Built in
+ #list dicts tuples and sets are non primitive meaning thry are classed as objects
+ #tuple is an immutable data structure
