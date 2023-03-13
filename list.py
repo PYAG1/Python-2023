8c88c50c986b89394c112c18855e946f2425e893
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-13 19:14:53 +0000

    lists in python

---
+++ list.py
@@ -0,0 +1,20 @@
+#lists are like arrays
+#you can nest lists
+
+list = [1,[1,2,3,4],5,6]
+
+
+#you can add the keyword 'sep='''to separate the list items
+
+
+#we can add by using insert but insert takes two parameters the first is like the index and the second is the value you want to add
+#you can use .append 
+#.extend([9,6,7,5]) adds an array of values
+list.insert(len(list),50)
+
+list.pop(1) #pop you mention the index
+
+print(list)
+
+for i in list:
+    print('hi',i)
